1b6727b6fe26b34f0c2faf0ff860b59f732aab48
Lucas Mota <lucasmota7@gmail.com>
2021-11-27 13:13:24 -0300

    Add project files.

---
+++ 14688 - Exercicio 26.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite o saldo inicial da conta: ");
+            double saldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do Deposito: ");
+            double deposito = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Valor do saque: ");
+            double saque = double.Parse(Console.ReadLine());
+
+            saldo += deposito - saque;
+
+            Console.Write("Saldo Atual da Conta: " + saldo.ToString("C"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
